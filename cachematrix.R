33ffff81841ff33b9fd120ddd47ff8c4b7fc230a
hessam612 <hessam.b612@gmail.com>
2015-07-23 20:43:57 +0330

    Coursera Programming Assignment 2
    These two functions calculate and cache the inverse of a matrix. The
    calculation function checks for the cached data and calculates the
    inverse only if cached data is not found.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions calculate and cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## This function does the caching part:
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+             set <- function(y) {
+                         x <<- y
+                         m <<- NULL
+                     }
+             get <- function() x
+             setsolve <- function(solve) m <<- solve
+             getsolve <- function() m
+             list(set = set, get = get,
+                             setsolve = setsolve,
+                             getsolve = getsolve)
 
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of the matrix. However, it first checks
+## for cached data and retrieves it if found.
 
 cacheSolve <- function(x, ...) {
+        ## Check for cached data
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
         ## Return a matrix that is the inverse of 'x'
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
